e42d856a12b99a2795d023693cb91e479eb758fe
Captainnnn <839822806@qq.com>
2020-07-18 15:59:17 +0800

    基本模块建好了

--- pages/index/index.vue
+++ pages/index/index.vue
@@ -1,28 +1,25 @@
 <template>
 	<view class="container">
-		<view class="intro">本项目已包含uni ui组件，无需import和注册，可直接使用。在代码区键入字母u，即可通过代码助手列出所有可用组件。光标置于组件名称处按F1，即可查看组件文档。</view>
-		<text class="intro">详见：</text>
-		<uni-link :href="href" :text="href"></uni-link>
+		首页
+		<swiper>
+			<swiper-item></swiper-item>
+		</swiper>
 	</view>
 </template>
 
 <script>
 	export default {
-		data() {
-			return {
-				href: 'https://uniapp.dcloud.io/component/README?id=uniui'
+		data(){
+			return{
+				
 			}
 		},
-		methods: {
-
+		methods:{
+			
 		}
 	}
 </script>
 
 <style>
-	.container {
-		padding: 20px;
-		font-size: 14px;
-		line-height: 24px;
-	}
+	
 </style>
